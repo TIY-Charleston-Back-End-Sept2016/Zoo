61563d4a6f332b300a9ea8e58601bb855f3d4952
oakes <zsoakes@gmail.com>
2016-09-29 09:35:32 -0400

    Add anonymous classes

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -13,6 +13,31 @@ public class Main {
         h.makeSound();
         s.makeSound();
 
+        // anonymous classes
+
+        Mammal d2 = new Mammal() {
+            @Override
+            void makeSound() {
+                System.out.println("Bark!");
+            }
+        };
+        Bird h2 = new Bird() {
+            @Override
+            void makeSound() {
+                System.out.println("Cawww!");
+            }
+        };
+        Reptile s2 = new Reptile() {
+            @Override
+            void makeSound() {
+                System.out.println("Ssssssss");
+            }
+        };
+
+        d2.makeSound();
+        h2.makeSound();
+        s2.makeSound();
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Type an animal name:");
         String name = scanner.nextLine();
